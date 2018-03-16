fb73b0b0e67a2fb88f4556c3fdca4cdab5ff974e
方觉 (Fang Jue) <fangjue23303@gmail.com>
2018-03-17 01:05:26 +0800

    Add a utility script to update embedded manifest in HTML

---
+++ update_embedded_manifest.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+begin_token="BEGIN MANIFEST"
+end_token="END MANIFEST"
+target_file="$dir/sz3.html"
+
+if ! diff -u <(grep -B 0 -A 99999 -F "$begin_token" "$target_file" | grep -B 99999 -A 0 "$end_token" | head -n -1 | tail -n +2) "$dir/manifest.json"; then
+  echo ======== Manifest changed! Updating... ========
+  ex -es -c '/BEGIN MANIFEST/+1,/END MANIFEST/-1 d' -c '/BEGIN MANIFEST/ r '"$dir/manifest.json" -c 'wq' "$target_file"
+fi
