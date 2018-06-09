8cd2829cdfdd2dc997ede5d4c3e9d3f78d3e8029
方觉 (Fang Jue) <fangjue23303@gmail.com>
2018-06-09 13:41:18 +0800

    Show both directions at the same time.

--- js/wj_timetable.js
+++ js/wj_timetable.js
@@ -1,10 +1,18 @@
 const lineSeparatorRegEx = /\r|\n|\r\n/;
+var allLines = {};
 var allData = {};
 
-fetch('/wj_all.guid').then(x => x.text()).then(data => {
+let fetchPromise = fetch('/wj_all.guid').then(x => x.text()).then(data => {
   let lineChooser = document.getElementById('lineChooser');
-  let lines = data.split(lineSeparatorRegEx).filter(x => x).map(x => x.split(/\t/));
-  fillSelect(lineChooser, lines.map(line => line[1] + ' (' + line[2] + ')'), lines.map(line => line[0]));
+  data.split(lineSeparatorRegEx).filter(x => x).map(x => x.split(/\t/)).forEach(line => {
+    let guid = line[0];
+    let name = line[1];
+    let direction = line[2];
+    if (!allLines[name])
+      allLines[name] = [];
+    allLines[name].push({direction: direction, guid: guid});
+  });
+  fillSelect(lineChooser, Object.keys(allLines).sort());
 }).then(_ =>
 fetch('/wj_timetable.csv')).then(x => x.text()).then(data => {
   document.getElementById('progress').style.display = 'none';
@@ -25,53 +33,119 @@ fetch('/wj_timetable.csv')).then(x => x.text()).then(data => {
 });
 
 document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('lineChooser').addEventListener('change', function() {
+    onLineChange();
+  });
+  document.getElementById('dateChooser').addEventListener('change', function() {
+    onDateChange();
+  });
+
+  fetchPromise.then(_ => onLineChange());
+});
+
+function defaultCompare(a, b) {
+  if (a > b)
+    return 1;
+  if (a < b)
+    return -1;
+  return 0;
+}
+
+function onLineChange() {
   let lineChooser = document.getElementById('lineChooser');
   let dateChooser = document.getElementById('dateChooser');
-  lineChooser.addEventListener('change', function() {
-    fillSelect(dateChooser, Object.keys(allData[lineChooser.value]).sort());
-  });
-  dateChooser.addEventListener('change', function() {
-    let timetable = allData[lineChooser.value][dateChooser.value].reduce((result, current) => {
-      if (!result[current.licenseId])
-        result[current.licenseId] = [];
-      result[current.licenseId].push(current.time);
-      return result;
-    }, {});
-    let order = Object.keys(timetable).sort((a, b) => {
-      if (timetable[a][0] > timetable[b][0])
-        return 1;
-      else if (timetable[a][0] < timetable[b][0])
-        return -1;
-      else
-        return 0;
-    });
+  fillSelect(dateChooser, Object.keys(allData[allLines[lineChooser.value][0].guid]).sort());
+  onDateChange();
+}
 
-    let table = document.createElement('table');
-    let tbody = document.createElement('tbody');
-    let tr = document.createElement('tr');
-    let th = document.createElement('th');
-    th.appendChild(document.createTextNode('车牌号'));
-    let th2 = document.createElement('th');
-    th2.colSpan = Object.keys(timetable).reduce((max, cur) => timetable[cur].length > max ? timetable[cur].length : max, 0);
-    th2.appendChild(document.createTextNode('发车时间'));
-    tr.appendChild(th);
-    tr.appendChild(th2);
-    tbody.appendChild(tr);
-    order.forEach(licenseId => {
-      let tr = document.createElement('tr');
-      let td = document.createElement('td');
-      td.appendChild(document.createTextNode(licenseId));
-      tr.appendChild(td);
-      timetable[licenseId].forEach(time => {
-        let td = document.createElement('td');
-        td.appendChild(document.createTextNode(time));
-        tr.appendChild(td);
-      });
-      tbody.appendChild(tr);
+function onDateChange() {
+  let lineChooser = document.getElementById('lineChooser');
+  let dateChooser = document.getElementById('dateChooser');
+  let timetable = {};
+  let directions = allLines[lineChooser.value];
+  directions.forEach((line, directionId) => {
+    allData[line.guid][dateChooser.value].forEach(current => {
+      if (!timetable[current.licenseId])
+        timetable[current.licenseId] = [];
+      timetable[current.licenseId].push({directionId: directionId, time: current.time});
     });
-    table.appendChild(tbody);
-    let content = document.getElementById('content');
-    removeChildren(content);
-    content.appendChild(table);
   });
-});
+  let order = Object.keys(timetable).sort((a, b) => {
+    return defaultCompare(a[0].directionId, b[0].directionId) ||
+      defaultCompare(a[0].time, b[0].time);
+  });
+  let maxCols = 0;
+  for (const [licenseId, runs] of Object.entries(timetable)) {
+    runs.sort((a, b) => defaultCompare(a.time, b.time));
+    let currentDirection = 0;
+    let runsCount = 0;
+    runs.forEach(details => {
+      if (currentDirection != details.directionId)
+        ++runsCount;
+      ++runsCount;
+      currentDirection = details.directionId == 1 ? 0 : 1;
+    });
+    maxCols = Math.max(maxCols, runsCount);
+  }
+
+  let table = document.createElement('table');
+  table.border = 1;
+  let tbody = document.createElement('tbody');
+  let tr = document.createElement('tr');
+  let th = document.createElement('th');
+  th.rowSpan = 2;
+  th.appendChild(document.createTextNode('车牌号'));
+  let th2 = document.createElement('th');
+  th2.colSpan = maxCols;
+  th2.appendChild(document.createTextNode('发车时间'));
+  tr.appendChild(th);
+  tr.appendChild(th2);
+  tbody.appendChild(tr);
+  let tr2 = document.createElement('tr');
+  let directionShortNames = [allLines[lineChooser.value][0].direction, allLines[lineChooser.value][1].direction];
+  for (let i = 0; i < Math.min(directionShortNames[0].length, directionShortNames[1].length); ++i) {
+    if (directionShortNames[0][i] == directionShortNames[1][i])
+      continue;
+    directionShortNames[0] = directionShortNames[0].substr(0, i + 1);
+    directionShortNames[1] = directionShortNames[1].substr(0, i + 1);
+  }
+  for (let i = 0; i < maxCols; ++i) {
+    let th = document.createElement('th');
+    let direction = allLines[lineChooser.value][i % 2];
+    th.title = direction.direction;
+    th.appendChild(document.createTextNode(directionShortNames[i % 2]));
+    tr2.appendChild(th);
+  }
+  tbody.appendChild(tr2);
+
+  order.forEach(licenseId => {
+    let tr = document.createElement('tr');
+    let td = document.createElement('td');
+    td.appendChild(document.createTextNode(licenseId));
+    tr.appendChild(td);
+    let currentDirection = 0;
+    let colCount = 0;
+    timetable[licenseId].forEach(details => {
+      if (currentDirection != details.directionId) {
+        let td = document.createElement('td');
+        tr.appendChild(td);
+        ++colCount;
+      }
+      currentDirection = details.directionId == 1 ? 0 : 1;
+      let td = document.createElement('td');
+      td.appendChild(document.createTextNode(details.time));
+      tr.appendChild(td);
+      ++colCount;
+    });
+    for (let i = 0; i < maxCols - colCount; ++i) {
+      let td = document.createElement('td');
+      tr.appendChild(td);
+    }
+    tbody.appendChild(tr);
+  });
+
+  table.appendChild(tbody);
+  let content = document.getElementById('content');
+  removeChildren(content);
+  content.appendChild(table);
+}
