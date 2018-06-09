c211e2bbc354612864975f3bc27271501286e27a
方觉 (Fang Jue) <fangjue23303@gmail.com>
2018-06-09 22:59:26 +0800

    Kill all background processes when Ctrl-C is pressed in start_dev.sh.

--- start_dev.sh
+++ start_dev.sh
@@ -3,10 +3,16 @@
 # ./node_modules/.bin/babel node_modules/whatwg-fetch/fetch.js --presets=env --out-file fetch.min.js --minified --source-maps
 
 pushd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null
+PIDS=()
 
 python -m SimpleHTTPServer &
+PIDS+=($!)
 ./node_modules/.bin/babel -w --presets=env --minified --source-maps --out-file timetable.min.js timetable.js &
+PIDS+=($!)
 ./node_modules/.bin/babel -w --presets=env --minified --source-maps --out-file js/wj_timetable.min.js js/common.js js/wj_timetable.js &
+PIDS+=($!)
+
+trap "echo ' Exiting...'; sleep 1; kill ${PIDS[*]} 2>/dev/null" SIGINT
 wait
 
-popd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null
+popd > /dev/null
