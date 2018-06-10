2ea474dc35e45fd17801099d18f3ec0e76698189
方觉 (Fang Jue) <fangjue23303@gmail.com>
2018-06-10 09:11:22 +0800

    Show aggregated timetable for a line across all dates.

--- js/wj_timetable.js
+++ js/wj_timetable.js
@@ -1,6 +1,26 @@
 const lineSeparatorRegEx = /\r|\n|\r\n/;
+const PALETTE = [
+  [230, 25, 75],
+  [60, 180, 75],
+  [255, 225, 25],
+  [0, 130, 200],
+  [245, 130, 48],
+  [145, 30, 180],
+  [70, 240, 240],
+  [240, 50, 230],
+  [210, 245, 60],
+  [250, 190, 190],
+  [0, 128, 128],
+  [170, 110, 40],
+  [128, 0, 0],
+  [170, 255, 195],
+  [128, 128, 0],
+  [0, 0, 128],
+].map(x => 'rgb(' + x.join(',') + ')');
+
 var allLines = {};
 var allData = {};
+var parsedLines = {};
 
 let fetchPromise = fetch('wj_all.guid').then(x => x.text()).then(data => {
   let lineChooser = document.getElementById('lineChooser');
@@ -32,34 +52,12 @@ fetch('wj_timetable.csv')).then(x => x.text()).then(data => {
   });
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-  document.getElementById('lineChooser').addEventListener('change', function() {
-    onLineChange();
-  });
-  document.getElementById('dateChooser').addEventListener('change', function() {
-    onDateChange();
-  });
-
-  fetchPromise.then(_ => onLineChange());
-});
-
 function defaultCompare(a, b) {
   if (a > b)
     return 1;
   if (a < b)
     return -1;
   return 0;
-}
-
-function onLineChange() {
-  let lineChooser = document.getElementById('lineChooser');
-  let dateChooser = document.getElementById('dateChooser');
-  let originalDate = dateChooser.value;
-  let dates = Object.keys(allData[allLines[lineChooser.value][0].guid]).sort();
-  fillSelect(dateChooser, dates);
-  if (dates.includes(originalDate))
-    dateChooser.value = originalDate;
-  onDateChange();
 }
 
 function parseByLineDate(line, date) {
@@ -93,16 +91,116 @@ function parseByLineDate(line, date) {
   return {maxCols: maxCols, data: new Map(order.map(k => [k, timetable[k]]))};
 }
 
+function parseLine(line) {
+  let dates = Array.from(allLines[line].reduce((result, cur) => (Object.keys(allData[cur.guid]).forEach(date => result.add(date)), result), new Set())).sort();
+  let parsedData = dates.map(date => Object.assign({date: date}, parseByLineDate(line, date))).
+      map(details => Object.assign(details, {key: Array.from(details.data.values()).map(runs => runs.map(run => run.directionId + '/' + run.time).join(',')).join(';')}));
+  let groupedData = new Map();
+  parsedData.forEach(day => {
+    if (groupedData.has(day.key)) {
+      groupedData.get(day.key).dates.set(day.date, Array.from(day.data.keys()));
+    } else {
+      groupedData.set(day.key, {dates: new Map([[day.date, Array.from(day.data.keys())]]), timetable: Array.from(day.data.values()), maxCols: day.maxCols});
+    }
+  });
+  return Array.from(groupedData.values());
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('lineChooser').addEventListener('change', function() {
+    onLineChange();
+  });
+  document.getElementById('dateChooser').addEventListener('change', function() {
+    onDateChange();
+  });
+
+  fetchPromise.then(_ => onLineChange());
+});
+
+function onLineChange() {
+  let lineChooser = document.getElementById('lineChooser');
+  let dateChooser = document.getElementById('dateChooser');
+  let originalDate = dateChooser.value;
+  let dates = ['显示所有日期'].concat(Object.keys(allData[allLines[lineChooser.value][0].guid]).sort());
+  fillSelect(dateChooser, dates, ['all']);
+  if (dates.includes(originalDate))
+    dateChooser.value = originalDate;
+  onDateChange();
+}
+
+function renderTimeTableRow(runs, maxCols) {
+  let tds = [];
+  let currentDirection = 0;
+  let colCount = 0;
+  runs.forEach(details => {
+    if (currentDirection != details.directionId) {
+      tds.push(createElement('td'));
+      ++colCount;
+    }
+    currentDirection = details.directionId == 1 ? 0 : 1;
+    tds.push(createElement('td', details.time));
+    ++colCount;
+  });
+  for (let i = 0; i < maxCols - colCount; ++i) {
+    tds.push(createElement('td'));
+  }
+  return tds;
+}
+
 function onDateChange() {
   let lineChooser = document.getElementById('lineChooser');
   let dateChooser = document.getElementById('dateChooser');
+  let content = document.getElementById('content');
+  if (dateChooser.value == 'all') {
+    let data = parseLine(lineChooser.value);
+    removeChildren(content);
+    data.forEach(cur => {
+      let header1 = createElement('tr', [
+        createElement('th', '日期', {rowSpan: 2}),
+        ...Array.from(cur.dates.keys()).map(date => createElement('th', date, {rowSpan: 2})),
+        createElement('th', '发车时间', {colSpan: cur.maxCols}),
+      ]);
+      let directions = allLines[lineChooser.value];
+      let directionShortNames = [directions[0].direction, directions[1].direction];
+      for (let i = 0; i < Math.min(directionShortNames[0].length, directionShortNames[1].length); ++i) {
+        if (directionShortNames[0][i] == directionShortNames[1][i])
+          continue;
+        directionShortNames[0] = directionShortNames[0].substr(0, i + 1);
+        directionShortNames[1] = directionShortNames[1].substr(0, i + 1);
+      }
+      let header2 = createElement('tr', new Array(cur.maxCols).fill(0).map((_, i) =>
+          createElement('th', directionShortNames[i % 2], {title: directions[i % 2].direction})));
+      let allLicenseIds = Array.from(Array.from(cur.dates.values()).reduce((set, cur) => (cur.forEach(licenseId => set.add(licenseId)), set), new Set())).sort();
+      appendChildren(content, [
+        createElement('table', [
+          createElement('thead', [header1, header2]),
+          createElement('tbody',
+            cur.timetable.map((runs, timetableIndex) => createElement('tr', [
+              ...(timetableIndex == 0 ? [createElement('th', '车牌号', {rowSpan: cur.timetable.length})] : []),
+              ...Array.from(cur.dates.values()).map(licenseIds => {
+                let lindex = allLicenseIds.indexOf(licenseIds[timetableIndex]);
+                let attr = {};
+                if (lindex >=0 && lindex < PALETTE.length)
+                  attr.style = 'background-color:' + PALETTE[lindex];
+                return createElement('td', licenseIds[timetableIndex], attr);
+              }),
+              ...renderTimeTableRow(runs, cur.maxCols),
+            ])),
+          ),
+        ], {border: 1}),
+        createElement('p'),
+      ]);
+    });
+    return;
+  }
+
   let {maxCols: maxCols, data: data} = parseByLineDate(lineChooser.value, dateChooser.value);
 
-  let headerBasic = document.createElement('tr', [
+  let headerBasic = createElement('tr', [
     createElement('th', '车牌号', {rowSpan: 2}),
     createElement('th', '发车时间', {colSpan: maxCols}),
   ]);
-  let headerDirections = document.createElement('tr');
+  let headerDirections = createElement('tr');
   let directions = allLines[lineChooser.value];
   let directionShortNames = [directions[0].direction, directions[1].direction];
   for (let i = 0; i < Math.min(directionShortNames[0].length, directionShortNames[1].length); ++i) {
@@ -116,7 +214,7 @@ function onDateChange() {
   }
   let thead = createElement('thead', [headerBasic, headerDirections]);
 
-  let tbody = document.createElement('tbody');
+  let tbody = createElement('tbody');
   for (const [licenseId, runs] of data) {
     let tr = createElement('tr', [createElement('td', licenseId)]);
     let currentDirection = 0;
