dac549dd7806b7d56549419516f353f6993d87c1
方觉 (Fang Jue) <fangjue23303@gmail.com>
2019-03-05 14:51:56 +0800

    Serve bus data from local cache until last_update_time changes in the manifest.

--- service_worker.js
+++ service_worker.js
@@ -1,7 +1,9 @@
 const ROOT = location.href.substring(0, location.href.lastIndexOf('/'));
 
-const CACHE_ALWAYS_CHECK = [
-  ROOT + '/manifest.json',
+const MANIFEST_URL = ROOT + '/manifest.json';
+
+const CACHE_ALWAYS_CHECK_LIST = [
+  MANIFEST_URL,
   ROOT + '/newbuses.csv',
 ];
 
@@ -9,6 +11,17 @@ const CACHE_NON_VOLATILE_LIST = [
   ROOT + '/archive/*',
   ROOT + '/images/szbus_*',
 ];
+
+const CACHE_MANAGED_LIST = [
+  ROOT + '/sz3.json',
+  ROOT + '/sz4.json',
+  ROOT + '/wj.json',
+];
+
+const HEADER_CACHE_ID = 'X-Service-Worker-Cache-Id';
+const HEADER_FALLBACK = 'X-Service-Worker-Fallback';
+
+let last_update_time = null;
 
 function urlMatch(url, list) {
   for (let pattern of list) {
@@ -23,35 +36,69 @@ function urlMatch(url, list) {
   }
 }
 
-function fetchAndCache(e) {
-  return fetch(e.request).then(r => {
-    let clonedResponse = r.clone();
-    e.waitUntil(caches.open('v1').then(cache => cache.put(e.request, clonedResponse)));
-    return r;
+function renewResponse(response, options) {
+  let newHeaders = options.headers || response.headers || {};
+  if (!(newHeaders instanceof Headers)) {
+    newHeaders = new Headers(response.headers);
+    for (let [name, value] of Object.entries(options.headers)) {
+      newHeaders.set(name, value);
+    }
+  }
+  return new Response(response.clone().body, {
+    status: options.status || response.status,
+    statusText: options.statusText || response.statusText,
+    headers: newHeaders,
   });
+}
+
+function fetchAndCache(request, e) {
+  let last_update_time_promise = CACHE_MANAGED_LIST.includes(request.url) ? getLastUpdateTime() : Promise.resolve();
+  return Promise.all([fetch(request), last_update_time_promise]).then(([response, last_update_time]) => {
+    let clonedResponse;
+    if (last_update_time) {
+      clonedResponse = renewResponse(response, {headers: {[HEADER_CACHE_ID]: last_update_time}});
+    } else {
+      clonedResponse = response.clone();
+    }
+    e.waitUntil(caches.open('v1').then(cache => cache.put(request, clonedResponse)));
+    if (request.url == MANIFEST_URL) {
+      response.clone().json().then(manifest => last_update_time = manifest.last_update_time);
+    }
+    return response;
+  });
+}
+
+function handleFetchDefault(request, e) {
+  return fetchAndCache(request, e).catch(_ =>
+      caches.match(new Request(request, {cache: 'default'})).then(response =>
+      response && renewResponse(response, {headers: {[HEADER_FALLBACK]: 1}})) );
+}
+
+function getLastUpdateTime() {
+  if (last_update_time)
+    return Promise.resolve(last_update_time);
+
+  return caches.match(new Request(MANIFEST_URL)).then(response => response ? response.json() : {}).
+      then(manifest => last_update_time = manifest.last_update_time);
 }
 
 self.addEventListener('fetch', function(e) {
   let request = e.request;
-  if (CACHE_ALWAYS_CHECK.includes(e.request.url)) {
+  if (CACHE_ALWAYS_CHECK_LIST.includes(request.url)) {
     request = new Request(request, {cache: 'no-cache'});
   }
 
-  if (urlMatch(e.request.url, CACHE_NON_VOLATILE_LIST)) {
-    e.respondWith(caches.match(e.request).then(r => r || fetchAndCache(e)));
-    return;
+  if (urlMatch(request.url, CACHE_NON_VOLATILE_LIST)) {
+    e.respondWith(caches.match(request).then(r => r || fetchAndCache(e)));
+  } else if (CACHE_MANAGED_LIST.includes(request.url)) {
+    e.respondWith(Promise.all([getLastUpdateTime(), caches.match(request.url)]).then(([last_update_time, response]) => {
+      if (response && last_update_time == response.headers.get(HEADER_CACHE_ID)) {
+        return response;
+      } else {
+        return handleFetchDefault(request, e);
+      }
+    }));
+  } else {
+    e.respondWith(handleFetchDefault(request, e));
   }
-
-  e.respondWith(fetchAndCache(e).catch(_ => caches.match(new Request(request, {cache: 'default'})).then(response => {
-    if (response) {
-      let newHeaders = new Headers(response.headers);
-      newHeaders.set('X-Service-Worker-Fallback', 1);
-      return new Response(response.body, {
-        status: response.status,
-        statusText: response.statusText,
-        headers: newHeaders,
-      });
-    }
-    return response;
-  })));
 });
